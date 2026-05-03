5224604217fead9621c2b54d8ad80912c055b42e
Flavio <contato.flaviobraga@gmail.com>
2026-05-03 17:33:12 -0300

    Ajusta relacionamentos no models.py conforme restrições de cardinalidade/participação

--- models.py
+++ models.py
@@ -8,7 +8,8 @@ class Unidade(db.Model):
     nome = db.Column(db.String(50), nullable=False)
     tipo = db.Column(db.String(20), nullable=False)
 
-    turmas = db.relationship('Turma', backref='unidade', lazy=True)
+    turmas = db.relationship('Turma', backref='unidade', lazy=True, cascade='all, delete-orphan')
+    colaboradores = db.relationship('Colaborador', backref='unidade', lazy=True, cascade='all, delete-orphan')
 
     def __init__(self, nome, tipo):
         self.nome = nome
@@ -19,12 +20,14 @@ class Atendido(db.Model):
     __tablename__ = 'atendidos'
 
     id_atendido = db.Column(db.Integer, primary_key=True)
+    id_turma = db.Column(db.Integer, db.ForeignKey('turmas.id_turma'), nullable=False)
     nome = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Boolean(), default=True, nullable=False)
 
-    frequencias = db.relationship('Frequencia', backref='atendido', lazy=True)
+    frequencias = db.relationship('Frequencia', backref='atendido', lazy=True, cascade='all, delete-orphan')
 
-    def __init__(self, nome, status=True):
+    def __init__(self, id_turma, nome, status=True):
+        self.id_turma = id_turma
         self.nome = nome
         self.status = status
 
@@ -33,15 +36,17 @@ class Colaborador(db.Model):
     __tablename__ = 'colaboradores'
 
     id_colaborador = db.Column(db.Integer, primary_key=True)
+    id_unidade = db.Column(db.Integer, db.ForeignKey('unidades.id_unidade'), nullable=False)
     nome_completo = db.Column(db.String(100), nullable=False)
     senha = db.Column(db.String(255), nullable=False)
     cargo = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     telefone = db.Column(db.String(20))
 
-    frequencias = db.relationship('Frequencia', backref='colaborador', lazy=True)
+    frequencias = db.relationship('Frequencia', backref='colaborador', lazy=True, cascade='all, delete-orphan')
 
-    def __init__(self, nome_completo, senha, cargo, email, telefone=None):
+    def __init__(self, id_unidade, nome_completo, senha, cargo, email, telefone=None):
+        self.id_unidade = id_unidade
         self.nome_completo = nome_completo
         self.senha = senha
         self.cargo = cargo
@@ -57,7 +62,8 @@ class Turma(db.Model):
     nome = db.Column(db.String(10), nullable=False)
     turno = db.Column(db.String(20), nullable=False)
 
-    frequencias = db.relationship('Frequencia', backref='turma', lazy=True)
+    atendidos = db.relationship('Atendido', backref='turma', lazy=True, cascade='all, delete-orphan')
+    frequencias = db.relationship('Frequencia', backref='turma', lazy=True, cascade='all, delete-orphan')
 
     def __init__(self, id_unidade, nome, turno):
         self.id_unidade = id_unidade
